eca4a5faf4fc25e7535b7b1fa0ae658e8315c270
Your Name <you@example.com>
2024-09-11 06:49:32 +0200

    Abgabe

--- Methoden.java
+++ Methoden.java
@@ -15,33 +15,60 @@ public class Methoden {
     }
     
     // TODO: Schreibe hier eine Methode für die Aufgabe c)
-    public static void sort(int e, int f, int g);
-    if (e > f > g) {
-        System.out.println( e " >" f ">" g);
-      }
-    if (g > f > e) {
-        System.out.println(g">"f">"e);
-      }
-    if (f > g > e) {
-        System.out.println(f">"g">"e);
-      }
-    if (f > e > g) {
-        System.out.println(f">"e">"g);
-      }
-    if (g > e > f) {
-        System.out.println(g">"e">"f);
-      }
-    if (e > g > f) {
-        System.out.println(e">"g">"f);
-      }
+    public static void sort(int e, int f, int g){
+        if (e > f && f > g) {
+            System.out.println( e + ">" + f + ">" + g);
+        }
+        if (e > g && g > f) {
+            System.out.println(e+">"+g+">"+f);
+        }
+        if (g > f && f > e) {
+            System.out.println(g + ">" + f + ">" + e);
+        }
+        if (g > e && e > f) {
+            System.out.println(g+">"+e+">"+f);
+        }
+        if (f > g && g > e) {
+            System.out.println(f+">"+g+">"+e);
+        }
+        if (f > e && e > g) {
+            System.out.println(f+">"+e+">"+g);
+        }
+        if (f == e && e > g) {
+            System.out.println(f+"="+e+">"+g);
+        }
+        if (f == g && g > e) {
+            System.out.println(f+"="+g+">"+e);
+        }
+        if (g == e && e > f) {
+            System.out.println(g+"="+e+">"+f);
+        }
+        if (f == e && e < g) {
+            System.out.println(g+">"+f+"="+e);
+        }
+        if (f == g && g < e) {
+            System.out.println(e+">"+f+"="+g);
+        }
+        if (g == e && e < f) {
+            System.out.println(f+">"+g+"="+e);
+        }
 
-
+        if (g == f && f == e) {
+            System.out.println(g+"="+f+"="+e);
+        }
+    }
 
 
     public static int distance(double x1, double y1, double x2, double y2){
         // TODO: implementiere hier deine Lösung für d)
         return 0; // diese Zeile darf verändert werden
+
+        
     }
+
+
+
+
 
         public static void main(String[] args){
         // Test-Code für Teilaufgabe a)
@@ -66,14 +93,14 @@ public class Methoden {
         // Test-Code für Teilaufgabe c)
         System.out.println("Aufgabe c)");
         // Test-Code ent-kommentieren, sobald die Funktion programmiert ist
-        // sort(1, 2, 3);
-        // sort(1, 3, 2);
-        // sort(2, 1, 3);
-        // sort(2, 3, 1);
-        // sort(3, 1, 2);
-        // sort(3, 2, 1);
-        // sort(1, 3, 3);
-        // sort(-1, -1, -1));
+        sort(1, 2, 3);
+        sort(1, 3, 2);
+        sort(2, 1, 3);
+        sort(2, 3, 1);
+        sort(3, 1, 2);
+        sort(3, 2, 1);
+        sort(1, 3, 3);
+        sort(-1, -1, -1);
         System.out.println();
 
         // Test-Code für Teilaufgabe d)
